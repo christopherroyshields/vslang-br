341b45a99e586048c5d86d50e1fc1fbb5c69096a
Chris <christopherroyshields@gmail.com>
2022-08-29 20:28:33 -0500

    Got next previous mostly working with built in commands.

--- out/extension.js
+++ out/extension.js
@@ -1,12 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
+const vscode = require("vscode");
 const lexi_1 = require("./lexi");
 function activate(context) {
     console.log('Extension "vslang-br" is now active!');
     (0, lexi_1.activateLexi)(context);
+    activateNextPrev(context);
 }
 exports.activate = activate;
 function deactivate() { }
 exports.deactivate = deactivate;
+function activateNextPrev(context) {
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand('vslang-br.nextOccurrence', (editor) => {
+        vscode.commands.executeCommand('editor.action.addSelectionToNextFindMatch');
+        vscode.commands.executeCommand('actions.find');
+        vscode.commands.executeCommand('toggleFindWholeWord');
+        vscode.commands.executeCommand('editor.action.nextMatchFindAction');
+        vscode.commands.executeCommand('toggleFindWholeWord');
+        vscode.commands.executeCommand('closeFindWidget');
+    }));
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand('vslang-br.prevOccurrence', (editor) => {
+        vscode.commands.executeCommand('editor.action.addSelectionToNextFindMatch');
+        vscode.commands.executeCommand('actions.find');
+        vscode.commands.executeCommand('toggleFindWholeWord');
+        vscode.commands.executeCommand('editor.action.previousMatchFindAction');
+        vscode.commands.executeCommand('toggleFindWholeWord');
+        vscode.commands.executeCommand('closeFindWidget');
+    }));
+}
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,29 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Extension "vslang-br" is now active!');
 
 	activateLexi(context);
+
+	activateNextPrev(context);
 }
 
 export function deactivate() {}
+
+function activateNextPrev(context: vscode.ExtensionContext) {
+  context.subscriptions.push(vscode.commands.registerTextEditorCommand('vslang-br.nextOccurrence', (editor) => {
+		vscode.commands.executeCommand('editor.action.addSelectionToNextFindMatch')
+		vscode.commands.executeCommand('actions.find')
+		vscode.commands.executeCommand('toggleFindWholeWord')
+		vscode.commands.executeCommand('editor.action.nextMatchFindAction')
+		vscode.commands.executeCommand('toggleFindWholeWord')
+		vscode.commands.executeCommand('closeFindWidget')
+  }))
+
+  context.subscriptions.push(vscode.commands.registerTextEditorCommand('vslang-br.prevOccurrence', (editor) => {
+		vscode.commands.executeCommand('editor.action.addSelectionToNextFindMatch')
+		vscode.commands.executeCommand('actions.find')
+		vscode.commands.executeCommand('toggleFindWholeWord')
+		vscode.commands.executeCommand('editor.action.previousMatchFindAction')
+		vscode.commands.executeCommand('toggleFindWholeWord')
+		vscode.commands.executeCommand('closeFindWidget')
+  }))
+}
 
