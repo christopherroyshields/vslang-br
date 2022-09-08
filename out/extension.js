45550a43f8e6818eeea9b344e6e2c28d73247061
Chris <christopherroyshields@gmail.com>
2022-09-07 23:55:18 -0500

    Added completion provider for statements and removed redundant snippets.

--- out/extension.js
+++ out/extension.js
@@ -1,14 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
+const vscode = require("vscode");
 const lexi_1 = require("./lexi");
 const next_prev_1 = require("./next-prev");
 const client_1 = require("./client");
+const statements_1 = require("./statements");
 function activate(context) {
-    console.log('Extension "vslang-br" is now active!');
     (0, lexi_1.activateLexi)(context);
     (0, next_prev_1.activateNextPrev)(context);
     (0, client_1.activateClient)(context);
+    vscode.languages.registerCompletionItemProvider({
+        language: "br",
+        scheme: "file"
+    }, {
+        provideCompletionItems: (doc, position) => {
+            let word = doc.getText(doc.getWordRangeAtPosition(position));
+            let isLower = !/[A-Z]/.test(word);
+            return statements_1.Statements.map((s) => {
+                let md = new vscode.MarkdownString();
+                let item = {
+                    label: {
+                        label: isLower ? s.name.toLocaleLowerCase() : s.name,
+                        description: 'statement'
+                    },
+                    detail: s.description,
+                    documentation: md,
+                    kind: vscode.CompletionItemKind.Keyword
+                };
+                if (s.documentation)
+                    md.appendMarkdown(s.documentation);
+                if (s.docUrl)
+                    md.appendMarkdown(` [docs...](${s.docUrl})`);
+                if (s.example)
+                    md.appendCodeblock(s.example);
+                return item;
+            });
+        }
+    });
+    console.log('Extension "vslang-br" is now active!');
 }
 exports.activate = activate;
 function deactivate() {
--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,49 @@ import * as vscode from 'vscode';
 import { activateLexi } from './lexi';
 import { activateNextPrev } from './next-prev';
 import { activateClient, deactivateClient } from './client'
+import { Statements } from './statements';
+import { CompletionItemLabelDetails } from 'vscode-languageclient';
+import { CompletionItemLabel } from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	
-	console.log('Extension "vslang-br" is now active!');
-
 	activateLexi(context);
 
 	activateNextPrev(context);
 
 	activateClient(context);
+
+	vscode.languages.registerCompletionItemProvider({
+		language: "br",
+		scheme: "file"
+	}, {
+		provideCompletionItems: (doc: vscode.TextDocument, position: vscode.Position): vscode.CompletionItem[] => {
+
+			let word = doc.getText(doc.getWordRangeAtPosition(position))
+			let isLower = !/[A-Z]/.test(word)
+			
+			return Statements.map((s)=>{
+				let md = new vscode.MarkdownString()
+				let item: vscode.CompletionItem = {
+					label: {
+						label: isLower ? s.name.toLocaleLowerCase() : s.name,
+						description: 'statement'
+					},
+					detail: s.description,
+					documentation: md,
+					kind: vscode.CompletionItemKind.Keyword
+				}
+				if (s.documentation) md.appendMarkdown(s.documentation)
+				if (s.docUrl) md.appendMarkdown(` [docs...](${s.docUrl})`)
+				if (s.example) md.appendCodeblock(s.example) 
+				return item
+			})
+
+		}
+	})
+
+	console.log('Extension "vslang-br" is now active!');
+
 }
 
 export function deactivate() {
