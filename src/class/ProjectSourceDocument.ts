ac73e8becb6930da07e454c3e755ec3da3b99db4
Chris <christopherroyshields@gmail.com>
2022-11-21 18:35:01 -0600

    Fixed issue with library link list function completions

--- src/class/ProjectSourceDocument.ts
+++ src/class/ProjectSourceDocument.ts
@@ -1,19 +1,20 @@
 import path = require("path");
-import { Uri, WorkspaceFolder } from "vscode";
+import { Uri, workspace, WorkspaceFolder } from "vscode";
 import { getSearchPath } from "../util/common";
 import BrSourceDocument from "./BrSourceDocument";
 
 export default class ProjectSourceDocument extends BrSourceDocument {
   uri: Uri
   workspaceFolder: WorkspaceFolder
-  /**
-   *
-   */
+  linkPath: string;
+
   constructor(text: string, uri: Uri, workspaceFolder: WorkspaceFolder) {
     super(text)
     this.uri = uri
     this.workspaceFolder = workspaceFolder
+    this.linkPath = workspace.asRelativePath(uri, false).replace("/","\\").replace(/\.[^\\/.]+$/,"")
   }
+
   private getLinkPath(workspaceFolder: WorkspaceFolder): string {
 		const searchPath = getSearchPath(workspaceFolder)
 		const parsedPath = path.parse(this.uri.fsPath.substring(searchPath.fsPath.length + 1))
