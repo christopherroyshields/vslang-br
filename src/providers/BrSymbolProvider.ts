bf6850bb2c2f1d7a5ad379783ece55f1b352c70e
Chris <christopherroyshields@gmail.com>
2022-11-28 08:51:22 -0600

    Removed unused variables.

--- src/providers/BrSymbolProvider.ts
+++ src/providers/BrSymbolProvider.ts
@@ -1,9 +1,6 @@
 import { CancellationToken, DocumentSymbol, DocumentSymbolProvider, Location, Position, ProviderResult, Range, SymbolInformation, SymbolKind, TextDocument } from "vscode"
 import BrSourceDocument from "../class/BrSourceDocument"
 
-const LABEL_SEARCH = /(?:\/\*[\s\S]+?(?:\*\/|$)|`[\s\S]+?(?:`|$)|(?:^|\n)\s*?(?:\d{0,5})(?:\s*)?(?<label>\w[\w\d]*):)/gi
-const FUNCTION_SEARCH = /(?:\/\*[\s\S]+?(?:\*\/|$)|`[\s\S]+?(?:`|$)|(?:^|\n)\s*?(?:\d{0,5})(?:\s*)?def\s+(?:library\s+)?(?<fn>\w[\w\d]*)\b)/gi
-
 export default class BrSourceSymbolProvider implements DocumentSymbolProvider {
   provideDocumentSymbols(doc: TextDocument, token: CancellationToken): SymbolInformation[] | DocumentSymbol[] {
     const symbolInfoList: DocumentSymbol[] = []
@@ -22,7 +19,6 @@ export default class BrSourceSymbolProvider implements DocumentSymbolProvider {
       symbolInfoList.push(symbolInfo)
     }
 
-    const funcMatches = doc.getText().matchAll(FUNCTION_SEARCH)
     for (const fn of brSource.functions) {
       const fullRange = new Range(doc.positionAt(fn.offset.start),doc.positionAt(fn.offset.end))
       const selectionRange = fullRange //doc.lineAt(doc.positionAt(fn.offset.start).line).range
