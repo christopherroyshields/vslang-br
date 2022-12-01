ca2187c93c649138e2dd253f7cbcdba0b8cc431e
Chris <christopherroyshields@gmail.com>
2022-11-30 22:52:27 -0600

    del project

--- src/providers/Project.ts
+++
@@ -1,7 +0,0 @@
-import ProjectSourceDocument from '../class/ProjectSourceDocument'
-import Layout from '../class/Layout'
-
-export type Project = {
-	sourceFiles: Map<string, ProjectSourceDocument>;
-	layouts: Map<string, Layout>;
-};
